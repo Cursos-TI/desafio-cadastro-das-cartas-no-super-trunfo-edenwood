10e9d5aff75b1506d75018871c8b95c5c5e18d75
Rogério Rocha <edenwood.france@gmail.com>
2025-02-28 18:02:11 +0000

    Adcionado comentarios para cada tipo de variavel

--- CartasSuperTrunfo.c
+++ CartasSuperTrunfo.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
 
 int main(){ 
-    char estado1, estado2; // 
-    char codigoEstado1, codigoEstado2;
-    char nomeCidade1, nomeCidade2;
-    int populacao1, populacao2;
-    float areaKm1, areaKm2;
-    float pib1, pib2;
-    int pontosTuristicos1, pontosTuristicos2;
+    char estado1, estado2; // Variáveis para armazenas os dados das cartas (A-H)
+    char codigoCidade1, codigoCidade2; // Armazena o código da cidade (ex: A01, B02)
+    int populacao1, populacao2; // Armazena a população das cidades correspondentes
+    float areaKm1, areaKm2; // Armazena a area em Km² das cidades correspondentes
+    float pib1, pib2; // Armazena o PIB das cidades correspondentes
+    int pontosTuristicos1, pontosTuristicos2; // Armazena a quantidade de pontos turísticos de cada cidade
 
 }
